7e06682903226dc02124023b7034789fc08986d7
Sebastián M <smunozmo@gmail.com>
2021-11-03 21:36:40 -0500

    Add methods a relations to models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_020527) do
+ActiveRecord::Schema.define(version: 2021_11_04_023037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 2021_11_04_020527) do
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.text "text"
+    t.bigint "post_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -52,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_020527) do
     t.integer "posts_counter"
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
